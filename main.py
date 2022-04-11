1a704dab7be26bd199ae5a05390a2c31f6c13b6b
Mauricio Mahmud <mauxms1996@gmail.com>
2022-04-11 00:35:11 -0400

    feat: SendSolution
    - Se mandasin problemas un archivo de texto al bot.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #!../env/bin/python3
 from os import environ
 
-from telegram.ext import Updater, CommandHandler, CallbackContext, MessegeHandler, Filters
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler , Filters
 
-from exception_handler import handleExceptions
+from exception_handler import handleExceptions, handleUnderConstruction
 
 TOKEN = environ["TOKEN"]
-
+     
 def main():
 
     updater = Updater(token=TOKEN, workers=8)
@@ -17,14 +18,17 @@ def main():
     dp.add_handler(CommandHandler("identify", identify_student))
     dp.add_handler(CommandHandler("help", help))
 
-    dp.add_handler(MessegeHandler(Filters.document, send_solution, run_async=True))
+    dp.add_handler(MessageHandler(Filters.document, send_solution, run_async=True))
 
+    updater.start_polling()
+    updater.idle()
 
 @handleExceptions
 def start(update : Update, context : CallbackContext):
     id = update.message.from_user.id 
     
     update.message.reply_text("Hola, bienvenido al bot de la asignatura de Modelos de Optimización 🤗🤗🤗.")
+    help(update,context)
    
 
     pass
@@ -34,30 +38,42 @@ def start(update : Update, context : CallbackContext):
 def send_solution(update : Update, context : CallbackContext):
     id = update.message.from_user.id 
      
+    solution = update.message.document.get_file().download_as_bytearray()
+    solution = str(solution,"utf8")
+
+    update.message.reply_text("Subiste esto :\n"+solution)
+    #
+    # Registrar 'solution' como la solucion asociada a un problema definido por el usuario :D
+    #
+
     pass
 
 @handleExceptions
+@handleUnderConstruction
 def identify_student(update : Update, context : CallbackContext):
     id = update.message.from_user.id 
 
     name, group = context.args[:2]
 
     print("Se autentico el usuario", name, "del grupo", group)
-    update.message.reply_text("Hola", nombre, "gracias por autenticarte 🤗.")
-
+    update.message.reply_text("Hola "+ name+ ", gracias por autenticarte 🤗.")
     pass
 
 @handleExceptions
 @handleUnderConstruction
 def help(update : Update, context : CallbackContext):
 
-    message = """
-    Primero debes autenticarte usando el comando
-    /auth <nombre> <grupo>
-    Usa un nombre que te identifique solo a ti en tu grupo y que no contenga espacios.  
+    message="- Primero debes autenticarte usando el comando\n"
+    message+="  /identify <nombre> <grupo>\n"
+    message+="  Usa un nombre que te identifique solo a ti en tu grupo y que no contenga espacios.\n\n"  
+    message+="- Luego puedes mandar las soluciones a en un *.txt, la primera linea debe tener el formato.\n"
+    message+="  # <id_del_problema> ..."
 
-    Luego puedes mandar las soluciones a en un *.txt, la primera linea debe tener el formato
-    # <id_del_problema> ...  
-    """
     update.message.reply_text(message)
     pass
+
+
+if __name__=="__main__":
+    print("Starting the MO bot...")
+    main()
+
